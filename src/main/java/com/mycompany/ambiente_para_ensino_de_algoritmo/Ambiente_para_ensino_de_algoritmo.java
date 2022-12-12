f68fa7041e319471d50242aa42806a4e5dda9b72
macaury <carvalhomacaury@gmail.com>
2022-12-12 16:57:33 -0300

    adicionando arquivos

---
+++ src/main/java/com/mycompany/ambiente_para_ensino_de_algoritmo/Ambiente_para_ensino_de_algoritmo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ambiente_para_ensino_de_algoritmo;
+
+/**
+ *
+ * @author macaurycarvalho
+ */
+public class Ambiente_para_ensino_de_algoritmo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
